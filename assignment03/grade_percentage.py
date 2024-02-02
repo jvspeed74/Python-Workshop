105827f6a90f666df945c1962b760c55b62df100
jvspeed74 <jvspeed74@gmail.com>
2024-02-02 13:43:27 -0500

    created get_input function

--- assignment03/grade_percentage.py
+++ assignment03/grade_percentage.py
@@ -1,15 +1,21 @@
 def get_input() -> float:
-    try:
-        user_input = float(input("Enter your grade percentage as a decimal: "))
 
+    # get user input
+    user_input = input("Enter your grade percentage as a decimal: ")
+
+    try:
+        # convert input to a float that's between 0 and 1
+        user_input = float(user_input)
         if user_input < 0 or user_input > 1:
             raise Exception(f"Invalid percentage: \'{user_input}\' => Value between 0 and 1 expected.")
 
-    except Exception as error:
-        print(error)
+    except ValueError:  # conversion error
+        exit(f'Invalid input: \'{user_input}\' => Expected numerical characters')
+    except Exception as error:  # percentage out-of-bounds error
+        exit(error)
 
-    else:
-        return user_input
+    # validated float between 0 and 1
+    return user_input
 
 
 get_input()
