2be4675e22223011d66224d218545b2e478e26d2
jvspeed74 <jvspeed74@gmail.com>
2024-02-02 23:12:20 -0500

    Refactored Directory Evaluated PigLatin.py documentation standards: PEP 8.

--- assignment02/PigLatin.py
+++ assignment02/PigLatin.py
@@ -2,11 +2,12 @@ def convert_to_piglatin(word: str) -> None:
     """
     Converts a word to Pig Latin by continuously moving the leading character to the end of the word
     until the character being moved is a consonant.
+
     :param word:
     """
 
     # decorator and script formatting
-    print("=" * 30)
+    print("=" * 20)
     print(f"Word: {word}")
     print(f"Piglatin: ", end="")
 
@@ -17,21 +18,27 @@ def convert_to_piglatin(word: str) -> None:
     # loop through characters
     for i in range(len(word)):
 
+        # add letter to stack
         stack += word[i]
 
-        # return if letter not in vowels
+        # if letter moved to stack is a consonant
         if word[i] not in vowels:
-            print(word[i+1:] + stack + "ay")
-            break
-
-    return
+            # print(rest of the word + letters previously iterated through + "ay")
+            print(word[i + 1:] + stack + "ay")
+            return
 
 
 def main():
+    """
+    Four test cases, which are english words, are passed into the convert_to_piglatin function.
+    The results get displayed as a consecutive output sequence.
+    A closing wrapper is attached after the final function call.
+    """
     convert_to_piglatin("simple")
     convert_to_piglatin("friends")
     convert_to_piglatin("omelet")
     convert_to_piglatin("ions")
+    print("=" * 20)
 
 
 main()
