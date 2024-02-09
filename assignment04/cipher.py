a9f03ce4b74cb062981c4b2ec8268d9029131222
jvspeed74 <jvspeed74@gmail.com>
2024-02-09 16:29:40 -0500

    added header statements to main script

--- assignment04/cipher.py
+++ assignment04/cipher.py
@@ -4,13 +4,32 @@ import cipher_module as cm
 
 def main():
     # TODO: Add decorators to program
+    # welcome statement
+    cm.print_section_header("Cipher")
+    print("Welcome! This program takes two inputs:\n"
+          "- The title of a passage in the library.\n"
+          "- A character to scan the passage for.")
 
     # initialize library handler and text scraper functionality
     text_scraper = cm.TextScraper()
+
+    cm.print_section_header("Select Text")
+    text_scraper.set_active_text()
+
+    # requirement loop: repeats until all conditions are met
     while True:
-        text_scraper.set_active_text()
+        cm.print_section_header(text_scraper.get_active_text())
         text_scraper.set_active_char()
-        print(text_scraper.get_char_count())
+
+        cm.print_section_header(text_scraper.get_active_text())
+        text_scraper.search_char()
+
+        if not text_scraper.get_char_count():
+            print("Character not found in text. Try another!")
+            continue
+
+        print(f"Your character {text_scraper.get_active_char()} occurs {text_scraper.get_char_count()} time(s)")
+        # all requirements met
         break
 
 
