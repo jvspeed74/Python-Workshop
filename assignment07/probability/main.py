c9a64c721784da84934b0464f8f87fa57868de3a
jvspeed74 <jvspeed74@gmail.com>
2024-03-11 01:20:23 -0400

    Refactored file directory and names

--- assignment07/probability/main.py
+++ assignment07/probability/main.py
@@ -5,3 +5,18 @@ File: probability.script.py
 Description: 
 Imports: 
 """
+
+import numpy as np
+
+
+def calculate_probability(dice_config, threshold):
+    num_dice, num_sides = map(int, dice_config.split("d"))
+    
+
+
+def main():
+    a = calculate_probability("1d20", 17)
+    print(a)
+
+
+main()
