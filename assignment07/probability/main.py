a3fe9bfc84de6b56fff5d58265fc7936f311db85
jvspeed74 <jvspeed74@gmail.com>
2024-03-11 19:19:54 -0400

    Completed script

--- assignment07/probability/main.py
+++ assignment07/probability/main.py
@@ -1,13 +1,11 @@
 """
 Author: Jalen Vaughn
 Date: 3/7/2024
-File: probability.script.py
-Description: 
-Imports: 
+File: probability.py
+Description: This script calculates the probability of rolling a die.
+Imports: product from itertools to generate a probability distribution
 """
 
-import numpy as np
-from scipy.stats import norm, triang, mode
 from itertools import product
 
 
@@ -15,17 +13,6 @@ class Utils:
     """
     This class contains utility/helper functions.
     """
-    
-    @staticmethod
-    def print_error(msg=None) -> None:
-        """
-        Prints an error message
-        :param msg: Error message to print
-        """
-        
-        Utils.print_header("Program Error")
-        if msg is not None:
-            print(msg)
     
     @staticmethod
     def print_header(header=None) -> None:
@@ -44,38 +31,66 @@ class Utils:
         print("=" * padding, header, "=" * padding)
 
 
-def probability(dice_config: str, threshold: int):
+def probability(dice_config: str, threshold: int) -> str:
     """
-    Calculates the probability of a die roll at and above the specified threshold
+    Calculates the probability of a die roll at and above the specified threshold.
     :param str dice_config: The dice configuration in the format "NdS" where N is the number of dice and S is the number
-     of sides
-    :param int threshold: The number of the target threshold to calculate at and above.
-    :return:
+     of sides.
+    :param int threshold: The number to calculate the probability at and above.
+    :return: String formatted probability of the dice roll.
     """
-    try:
+    
+    try:  # Catch errors and validate the dice_config input
         if type(dice_config) is not str:
-            raise ValueError("Dice configuration must be a string consisting of two numbers delimited by \"d\"")
-        elif type(threshold) is not int or threshold < 0:
-            raise ValueError("Threshold must be a positive integer")
+            raise ValueError("Dice configuration must be a string\nconsisting of two positive integers "
+                             "delimited by \"d\"")
+        elif type(threshold) is not int or threshold <= 0:
+            raise ValueError("Threshold must be a non-zero positive integer")
         
+        # Split dice_config by the delimiter into two integer variables
         num_dice, num_sides = map(int, dice_config.split("d"))
+        
+        # Prevent divide by zero error
+        if num_sides <= 0:
+            raise ValueError("A zero sided dice configuration cannot exist.")
     
     except ValueError as e:
-        Utils.print_error()
-        return e
+        return f"Program Error: {e}"
+    
     else:
+        # Calculate the total amount of combinations in the set
+        total_events: int = num_sides ** num_dice
         
-        total_events = num_sides ** num_dice
+        # Generate a set of all possible combinations
+        sample_space: product[tuple[int, ...]] = product(range(1, num_sides + 1), repeat=num_dice)
         
-        sample_space = product(range(1, num_sides + 1), repeat=num_dice)
+        # Iterate through the set. The count of events in the sample space that meet the threshold requirement are
+        # summed up and declared as target_events
+        target_events: int = sum(sum(e) >= threshold for e in sample_space)
         
-        possible_events = sum(sum(e) >= threshold for e in sample_space)
-        
-        return possible_events / total_events * 100
+        # Return the chances of rolling at or above the threshold
+        return f"{target_events / total_events * 100:.2f}%"
 
 
 def main():
-    print(probability("52d6", "6"))
+    """
+    Main entry point of the program. Each test case within the program will run through the target function and print
+    the result.
+    """
+    test_cases = [
+        ('2d6', 7),
+        ('2d6', 9),
+        ('3d8', 22),
+        ('1d20', 17),
+        (6, 5)
+    ]
+    
+    # run parameters through the probability function
+    for dc, t in test_cases:
+        Utils.print_header(f"Test: {dc}, {t}")
+        print(probability(dc, t))
+    else:
+        Utils.print_header()
 
 
 main()
