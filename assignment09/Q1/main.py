9993989742192f789002165e1686c8f3aa2b1e84
jvspeed74 <javaugh@iu.edu>
2024-03-26 19:47:58 -0400

    Added test cases

--- assignment09/Q1/main.py
+++ assignment09/Q1/main.py
@@ -5,22 +5,28 @@ File: main.py
 Description: This script contains the solution to question 1
 Dependencies/Imports: None
 """
-from typing_extensions import a
+
 
 class Subgrid:
-    def __init__(self, grid: list[list[int]]):
-        self.grid = grid
-    
-    def is_matrix(self):
-        pass
-    
-    def is_list(self):
+    def __init__(self):
         pass
 
 
 def main():
-    test = Subgrid([[5, 3, 4], [6, 7, 2], [1, 9, 8]])
-    print(test.validate())
+    # Declare test cases
+    test_cases = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [[5, 3, 4], [6, 7, 2], [1, 9, 8]],
+        [[9, 9, 2], [3, 4, 8], [5, 6, 7]],
+        [[8, 5, 9], [4, 2, 6], [8, 1, 3]],
+        [[7, 6, 1], [6, 8, 5], [3, 9, 6]],
+        [[4, 2, "E"], [7, 9, "I"], [8, "S", 6]],
+        [[6, 7, 8], [1, 9, 5, 3], [3, 4, 2]]
+    ]
+    
+    # Call each test case and display result
+    for test_case in test_cases:
+        ...  # do something
 
 
 main()
